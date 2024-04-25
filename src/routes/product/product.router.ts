c90eac866dfa43281e17b334c8a0a42f9afeedfa
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2024-04-24 22:15:25 -0300

    feat: Added get product router

--- src/routes/product/product.router.ts
+++ src/routes/product/product.router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { CreateProductMongoRepository } from "../../repositories/create-product/create-product-mongo.repository";
 import { CreateProductController } from "../../controller/create-product/create-product.controller";
+import { GetProductsMongoRepository } from "../../repositories/get-products/get-products-mongo.repository";
+import { GetProductsController } from "../../controller/get-products/get-products.controller";
 
 const productRouter = Router();
 
@@ -15,6 +17,17 @@ productRouter.post("", async (req, res) => {
   const createdProduct = await createProductController.handle({ body });
 
   return res.status(createdProduct.statusCode).json(createdProduct.body);
+});
+
+productRouter.get("", async (req, res) => {
+  const getProductsRepository = new GetProductsMongoRepository();
+  const getProductsController = new GetProductsController(
+    getProductsRepository
+  );
+
+  const products = await getProductsController.handle();
+  
+  return res.status(products.statusCode).json(products.body);
 });
 
 export { productRouter };
