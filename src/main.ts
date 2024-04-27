c95fe79ba5133a3dcd98429106bb00e86db053f9
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2024-04-27 15:14:27 -0300

    feat: Changed main to init AWS SDK on application start correctly

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { config } from "dotenv";
 import app from "./app";
 import { Logger } from "./utils/logger";
 import { MongoDBClient } from "./database/mongo";
+import { AwsSingleton } from "./external/aws";
 
 const startServer = async () => {
   config();
   const port = process.env.PORT || 8080;
   const logger = new Logger("info", "server");
   await MongoDBClient.connect();
+  AwsSingleton.init();
   app.listen(port, () => {
     logger.info(`Server started on port ${port}`);
   });
