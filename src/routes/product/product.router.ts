d18609b2922e352a79d6f09b2c875ebd284c45d0
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2024-04-26 21:44:11 -0300

    feat: added find product route

--- src/routes/product/product.router.ts
+++ src/routes/product/product.router.ts
@@ -5,6 +5,8 @@ import { GetProductsMongoRepository } from "../../repositories/get-products/get-
 import { GetProductsController } from "../../controller/get-products/get-products.controller";
 import { UpdateProductMongoRepository } from "../../repositories/update-product/update-product-mongo.repository";
 import { UpdateProductController } from "../../controller/update-product/update-product.controller";
+import { FindProductMongoRepository } from "../../repositories/find-product/find-product-mongo.repository";
+import { FindProductController } from "../../controller/find-product/find-product.controller";
 
 const productRouter = Router();
 
@@ -42,6 +44,18 @@ productRouter.patch("/:id", async (req, res) => {
   const updatedProduct = await updateProductController.handle({ params, body });
 
   return res.status(updatedProduct.statusCode).json(updatedProduct.body);
+});
+
+productRouter.get("/:id", async (req, res) => {
+  const findProductRepository = new FindProductMongoRepository();
+  const findProductController = new FindProductController(
+    findProductRepository
+  );
+  const { params } = req;
+
+  const product = await findProductController.handle({ params });
+
+  return res.status(product.statusCode).json(product.body);
 });
 
 export { productRouter };
